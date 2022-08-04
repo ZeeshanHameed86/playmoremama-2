284d1f9aba247a34aafd5c1d3a3a5833a5accba9
zeshanhameed86@gmail.com <zeshanhameed86@gmail.com>
2022-08-04 23:30:56 +0500

    section 1 completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import sideline1 from "../assets/Sideline-1.png";
 import sideline2 from "../assets/Sideline-2.png";
 import headerimg from "../assets/Header-image.jpg";
+import rainbowimg from "../assets/Rainbow.png";
 
 const Header = () => {
   return (
@@ -11,6 +12,10 @@ const Header = () => {
       <div className="header-container">
         <div className="image-container">
           <img src={headerimg} alt="" className="header-img" />
+        </div>
+        <h3>Monterey Bay, CA</h3>
+        <div className="rainbow-img-container">
+          <img src={rainbowimg} alt="" className="rainbow-img" />
         </div>
         <div className="header-info">
           <h1>Welcome</h1>
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,14 +1,62 @@
 import React from "react";
 import sideline1 from "../assets/Sideline-1-2.png";
 import sideline2 from "../assets/Sideline-2-2.png";
+import world from "../assets/World.png";
+import brain from "../assets/Brain.png";
+import hands from "../assets/Hands.png";
 
 const Introduction = () => {
   return (
-    <div className="Introduction">
+    <section className="intro">
       <img src={sideline1} alt="" className="intro-sideline-img-1" />
       <img src={sideline2} alt="" className="intro-sideline-img-2" />
-      hello
-    </div>
+      <div className="intro-details">
+        <h1 className="intro-title">Say Hello to Calm, Happy, and</h1>
+        <h1 className="intro-title intro-subtitle">Creative Experiences</h1>
+        <div className="intro-layout">
+          <div className="intro-main-detail">
+            <img src={world} alt="" className="img-1" />
+            <div className="main-detail-text">
+              <h2>Just For You</h2>
+              <div>
+                <p>
+                  Sensory rich play is inclusive for everyone in any
+                  environment. Indoors - check. Outdoors - check. On the moon?
+                  Probably. (Just need the right training and equipment!).
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="intro-main-detail">
+            <div className="main-detail-text">
+              <h2>And Your Brain</h2>
+              <div>
+                <p>
+                  While you play, connections on the brain's pathways are being
+                  made which lead to more complex learning tasks. Memory,
+                  language, and cognitive development improve. High-fives for
+                  learning!
+                </p>
+              </div>
+            </div>
+            <img src={brain} alt="" />
+          </div>
+          <div className="intro-main-detail">
+            <img src={hands} alt="" />
+            <div className="main-detail-text">
+              <h2>And Your Body</h2>
+              <div>
+                <p>
+                  A body is made up of lots of muscles that can become tense or
+                  agitated. Sensory play helps calm the body while developing
+                  fine and gross motor skills.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
