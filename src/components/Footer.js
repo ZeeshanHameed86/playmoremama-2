2f26eca3fb703ceac43fb97cdb1987030bceee19
zeshanhameed86@gmail.com <zeshanhameed86@gmail.com>
2022-08-07 16:41:44 +0500

    HomePage modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
 import React from "react";
 import img1 from "../assets/Footer-Image-1.jpg";
 import img2 from "../assets/Footer-Image-2.jpg";
+import hearts from "../assets/Hearts.png";
+import flowers from "../assets/Flowers.png";
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer-title-container">
-        <h1 className="footer-title">How Do You Sensory?</h1>
-        <h1 className="footer-title">Tag Us @play.more.mama</h1>
+        <div></div>
+        <div>
+          <h1 className="footer-title">How Do You Sensory?</h1>
+          <h1 className="footer-title">Tag Us @play.more.mama</h1>
+        </div>
+        <div className="footer-title-img-container">
+          <img src={hearts} alt="" />
+        </div>
       </div>
       <div className="insta-posts">
         <div className="footer-img-left">
@@ -18,7 +26,13 @@ const Footer = () => {
           <img src={img2} alt="" />
         </div>
       </div>
-      <h1 className="footer-brand">play.more.mama</h1>
+      <div className="footer-brand-container">
+        <div className="footer-brand-img-container">
+          <img src={flowers} alt="" />
+        </div>
+        <h1 className="footer-brand">play.more.mama</h1>
+        <div></div>
+      </div>
       <div className="footer-bottom">
         <section className=" footer-bottom-container">
           <div className="footer-bottom-main section-center">
