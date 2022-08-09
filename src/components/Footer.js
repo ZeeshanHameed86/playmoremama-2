1290fbafc25a14bc86c999b5007853dc79478533
zeshanhameed86@gmail.com <zeshanhameed86@gmail.com>
2022-08-10 01:21:50 +0500

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -36,11 +36,15 @@ const Footer = () => {
       <div className="footer-bottom">
         <section className=" footer-bottom-container">
           <div className="footer-bottom-main section-center">
-            <div className="footer-bottom-left">Privacy Policy</div>
-            <div className="footer-bottom-middle">
-              Copyright &copy; 2022, play.more.mama
+            <div className="footer-bottom-left">
+              <h3>Privacy Policy</h3>
             </div>
-            <div className="footer-bottom-right">Terms and Conditions</div>
+            <div className="footer-bottom-middle">
+              <h3>Copyright &copy; 2022, play.more.mama</h3>
+            </div>
+            <div className="footer-bottom-right">
+              <h3>Terms and Conditions</h3>
+            </div>
           </div>
         </section>
       </div>
